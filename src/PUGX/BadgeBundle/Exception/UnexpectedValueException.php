ad7a6ab7a8fba8be4f87d924be6e1a62d3f58de5
Giulio De Donato <liuggio@gmail.com>
2013-05-28 13:56:44 +0200

    added MIT license

--- src/PUGX/BadgeBundle/Exception/UnexpectedValueException.php
+++ src/PUGX/BadgeBundle/Exception/UnexpectedValueException.php
@@ -1,4 +1,13 @@
 <?php
+
+/*
+ * This file is part of the badge-poser package
+ *
+ * (c) Giulio De Donato <liuggio@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace PUGX\BadgeBundle\Exception;
 
 class UnexpectedValueException
--- src/PUGX/BadgeBundle/Tests/Controller/BadgeControllerTest.php
+++ src/PUGX/BadgeBundle/Tests/Controller/BadgeControllerTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the badge-poser package
+ *
+ * (c) Giulio De Donato <liuggio@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace PUGX\BadgeBundle\Tests\Controller;
 
 use Packagist\Api\Client;
