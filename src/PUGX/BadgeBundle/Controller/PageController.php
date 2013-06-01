a1f4cc09a1ddd421f6cda0c56cf5ba79cea6e307
Giorgio Cefaro <giorgio.cefaro@gmail.com>
2013-06-01 11:34:46 +0200

    Bootstrap homepage added. partially solves #9

---
+++ src/PUGX/BadgeBundle/Controller/PageController.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the badge-poser package
+ *
+ * (c) Giulio De Donato <liuggio@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PUGX\BadgeBundle\Controller;
+
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class PageController extends ContainerAware
+{
+    /**
+     * @Route("/",
+     *     name="pugx_page_home"
+     *     )
+     * @Method({"GET"})
+     * @Template
+     * @Cache(smaxage="3600")
+     * @return Response
+     */
+    public function homeAction()
+    {
+        return array();
+    }
+}
---
+++ src/PUGX/BadgeBundle/Tests/Controller/PageControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the badge-poser package
+ *
+ * (c) Giulio De Donato <liuggio@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PUGX\BadgeBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PageControllerTest extends WebTestCase
+{
+    public function testHomeAction()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals(1, $crawler->filter('#container h1:contains("Badge Poser")')->count());
+        $this->assertEquals(1, $crawler->filter('#container p:contains("Pimp your README!")')->count());
+    }
+}
