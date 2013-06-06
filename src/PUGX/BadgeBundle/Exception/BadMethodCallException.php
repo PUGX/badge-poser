82ca9817874ad513ae342371cea3625738469a86
eux <euxpom@gmail.com>
2013-06-06 22:52:36 +0200

    substitution of the old license with the new one inside the classes

--- src/PUGX/BadgeBundle/Exception/BadMethodCallException.php
+++ src/PUGX/BadgeBundle/Exception/BadMethodCallException.php
@@ -1,12 +1,14 @@
 <?php
+
 /*
- * This file is part of the badge-poser package
+ * This file is part of the badge-poser package.
  *
- * (c) Giulio De Donato <liuggio@gmail.com>
+ * (c) PUGX <http://pugx.github.io/>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace PUGX\BadgeBundle\Exception;
 
 class BadMethodCallException
--- src/PUGX/StatsBundle/DependencyInjection/Configuration.php
+++ src/PUGX/StatsBundle/DependencyInjection/Configuration.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the badge-poser package.
+ *
+ * (c) PUGX <http://pugx.github.io/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace PUGX\StatsBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
