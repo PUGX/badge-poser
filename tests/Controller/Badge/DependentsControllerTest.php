5b109e759abcee6091a8165eaf866ade01d45791
chadicus <chadwickgray@gmail.com>
2018-03-08 14:16:58 -0500

    Add DependentsController

---
+++ tests/Controller/Badge/DependentsControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the badge-poser package.
+ *
+ * (c) PUGX <http://pugx.github.io/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Basge\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Class DependentsControllerTest
+ * @package App\Tests\Basge\Controller
+ */
+class DependentsControllerTest extends WebTestCase
+{
+    public function testDependentsAction()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/pugx/badge-poser/dependents');
+        $this->assertTrue($client->getResponse()->isSuccessful(), $client->getResponse()->getContent());
+    }
+
+    public function testDependentsActionSvgExplicit()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/pugx/badge-poser/dependents.svg');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function testDependentsActionPngRedirectSvg()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/pugx/badge-poser/dependents.png');
+        $crawler = $client->followRedirect();
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertContains('/pugx/badge-poser/dependents', $crawler->getUri());
+    }
+}
