47b27050f25ac0e060d9ec47a08c4ac99cad3001
Andrea Giannantonio <a.giannantonio@gmail.com>
2020-05-19 18:00:30 +0200

    fix(quote): i hate use double quote

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-import * as Sentry from '@sentry/browser';
+import * as Sentry from "@sentry/browser";
 import Awesomplete from "awesomplete";
 import {debounce} from "./debounce";
 import "./clipboard";
