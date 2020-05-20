9d7086e6d92349ff8979160325c4022556345ff2
Giordano Ricci <me@giordanoricci.com>
2020-05-20 19:17:53 +0100

    chore: update deps & move to tippy.js

--- assets/js/clipboard.js
+++ assets/js/clipboard.js
@@ -1,15 +1,16 @@
 import Clipboard from "clipboard";
-import Tooltip from "tooltip.js";
+import tippy from "tippy.js";
 
 const clipboard = new Clipboard("button");
 
 clipboard.on("success", (e) => {
-    const instance = new Tooltip(e.trigger, {
-        title: "Copied!",
-        placement: "top",
-        trigger: "manual",
-    });
-    instance.show();
+  const instance = tippy(e.trigger, {
+    content: "Copied!",
+    placement: "top",
+    showOnCreate: true,
+    trigger: "manual",
+    theme: "translucent",
+  });
 
-    setTimeout(() => instance.dispose(), 2000);
+  setTimeout(instance.destroy, 2000);
 });
