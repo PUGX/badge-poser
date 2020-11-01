b612a122991b1d17af0ed37c34e88b9f4d4a107e
fabiocicerchia <info@fabiocicerchia.it>
2020-11-01 19:31:08 +0100

    improved regex

--- sys/nginx/lua/hook.lua
+++ sys/nginx/lua/hook.lua
@@ -1,4 +1,16 @@
-local regex = [[/(circleci|circleci/.+|circleci/.+\.svg|circleci\.svg|composerlock|composerlock\.svg|d/daily|d/daily\.png|d/daily\.svg|d/monthly|d/monthly\.png|d/monthly\.svg|d/total|d/total\.png|d/total\.svg|dependents|dependents\.svg|downloads|downloads\.png|downloads\.svg|gitattributes|gitattributes\.svg|license|license\.png|license\.svg|suggesters|suggesters\.svg|v/stable|v/stable\.png|v/stable\.svg|v/unstable|v/unstable\.png|v/unstable\.svg|version|version\.png|version\.svg)$]]
+local regex = [[/(]]
+regex = regex .. [[circleci(/.+)?(\.svg)?]]
+regex = regex .. [[|composerlock(\.svg)?]]
+regex = regex .. [[|d/(daily|monthly|total)(\.svg|\.png)?]]
+regex = regex .. [[|dependents(\.svg)?]]
+regex = regex .. [[|downloads(\.svg|\.png)?]]
+regex = regex .. [[|gitattributes(\.svg)?]]
+regex = regex .. [[|license(\.svg|\.png)?]]
+regex = regex .. [[|suggesters(\.svg)?]]
+regex = regex .. [[|v/(stable|unstable)(\.svg|\.png)?]]
+regex = regex .. [[|version(\.svg|\.png)?]]
+regex = regex .. [[)$]]
+
 local m = ngx.re.match(ngx.var.request_uri, regex)
 if not m then return end
 
